3ee8d45a88840daf754068afd3af589753c48583
Sarah Abdelfattah <sarah_23s@hotmail.com>
2020-07-07 04:19:34 +0200

    layout and styles

--- src/client/app.js
+++ src/client/app.js
@@ -1,2 +1,12 @@
 // import { checkForName } from './js/nameChecker'
 // import { handleSubmit } from './js/formHandler'
+
+import './styles/main.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+import plannerImg from './images/planner.jpg'
+import cloudsImg from './images/clouds.jpeg'
+import globeImg from './images/globe.png'
+
+
